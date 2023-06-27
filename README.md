f97f193cc2a1b1a1f21dd6756f131e2947a11097
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2023-06-27 17:53:08 +0200

    DOC: Ajout du readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# My Awesome AI-Tetris
+# Lunar Lander Q-Learning attempt
 
+This is my attempt to solve the Lunar Lander problem using Q-Learning.
+
+## How to use this code
+
+All the parameters can be indicated on the command line like this:
+ 
+```bash
+python main.py --hidden_layers="[64, 64]" --learning_rate=0.001 --epoch=1000 --gamma=0.99 --epsilon=1.0 --epsilon_dec=0.999 --epsilon_end=0.01 --mem_size=100000 --batch_size=64 --activation_function="relu" --render_during_training=True --live_plot=True
+```
+
+If you not indicate any parameter, the default value will be used. All the defaults the parameters is in the top of the main.py file.
+
+If there is already a model saved, (on the folder `models`), the program will skip the training and will play the game using the model.
--- main.py
+++ main.py
@@ -1,46 +1,47 @@
 import argparse
 from termcolor import colored
 
+# Default arguments
+ENV_NAME = "LunarLander-v2"
+HIDDEN_LAYERS=[150, 120]
+LEARNING_RATE=.001
+EPOCH=30000
+GAMMA=.99
+EPSILON=1.0
+EPSILON_DEC=.996
+EPSILON_END=0.01
+MEM_SIZE=1000000
+BATCH_SIZE=64
+ACTIVATION_FUNCTION = "linear"
+RENDER_DURING_TRAINING = False
+LIVE_PLOT = True
+
+
 print("\nLoading Tensorflow...", end="\r")
 from src.QAgent import QAgent
 print(f"Loading Tensorflow... {colored('Done', 'green')}\n")
 
 def load_arguments():
-  # Default arguments
-  env_name = "LunarLander-v2"
-  hidden_layers = [150, 120]
-  learning_rate=.001
-  epoch=30000
-  gamma=.99
-  epsilon=1.0
-  epsilon_dec=.996
-  epsilon_end=0.01
-  mem_size=1000000
-  batch_size=64
-  activation_function = "linear"
-  render_during_training = False
-  live_plot = True
 
   # On prent les arguments
   parser = argparse.ArgumentParser(description='Q-Learning')
-  parser.add_argument('--env_name', type=str, default=env_name, help='Environment name')
-  parser.add_argument('--hidden_layers', type=list, default=hidden_layers, help='Hidden layers')
-  parser.add_argument('--learning_rate', type=float, default=learning_rate, help='Learning rate')
-  parser.add_argument('--epoch', type=int, default=epoch, help='Epoch')
-  parser.add_argument('--gamma', type=float, default=gamma, help='Gamma')
-  parser.add_argument('--epsilon', type=float, default=epsilon, help='Epsilon')
-  parser.add_argument('--epsilon_dec', type=float, default=epsilon_dec, help='Epsilon dec')
-  parser.add_argument('--epsilon_end', type=float, default=epsilon_end, help='Epsilon end')
-  parser.add_argument('--mem_size', type=int, default=mem_size, help='Memory size')
-  parser.add_argument('--batch_size', type=int, default=batch_size, help='Batch size')
-  parser.add_argument('--activation_function', type=str, default=activation_function, help='Activation function')
-  parser.add_argument('--render_during_training', type=bool, default=render_during_training, help='Render during training')
-  parser.add_argument('--live_plot', type=bool, default=live_plot, help='Live plot')
+  parser.add_argument('--hidden_layers', type=list, default=HIDDEN_LAYERS, help='Hidden layers')
+  parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE, help='Learning rate')
+  parser.add_argument('--epoch', type=int, default=EPOCH, help='Epoch')
+  parser.add_argument('--gamma', type=float, default=GAMMA, help='Gamma')
+  parser.add_argument('--epsilon', type=float, default=EPSILON, help='Epsilon')
+  parser.add_argument('--epsilon_dec', type=float, default=EPSILON_DEC, help='Epsilon dec')
+  parser.add_argument('--epsilon_end', type=float, default=EPSILON_END, help='Epsilon end')
+  parser.add_argument('--mem_size', type=int, default=MEM_SIZE, help='Memory size')
+  parser.add_argument('--batch_size', type=int, default=BATCH_SIZE, help='Batch size')
+  parser.add_argument('--activation_function', type=str, default=ACTIVATION_FUNCTION, help='Activation function')
+  parser.add_argument('--render_during_training', type=bool, default=RENDER_DURING_TRAINING, help='Render during training')
+  parser.add_argument('--live_plot', type=bool, default=LIVE_PLOT, help='Live plot')
 
  
   args = parser.parse_args()
 
-  return args.env_name, args.hidden_layers, args.learning_rate, args.epoch, args.gamma, args.epsilon, args.epsilon_dec, args.epsilon_end, args.mem_size, args.batch_size, args.activation_function, args.render_during_training, args.live_plot
+  return ENV_NAME, args.hidden_layers, args.learning_rate, args.epoch, args.gamma, args.epsilon, args.epsilon_dec, args.epsilon_end, args.mem_size, args.batch_size, args.activation_function, args.render_during_training, args.live_plot
 
 if __name__ == "__main__":
   
